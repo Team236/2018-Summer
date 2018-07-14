bf8e98596699028be690573dc66d59d86864587a
Unknown <speckhalse@region18.org>
2018-07-13 21:26:09 -0400

    Add DriveWithJoysticks command

---
+++ src/org/usfirst/frc/team236/robot/commands/DriveWithJoysticks.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team236.robot.commands;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveWithJoysticks extends Command {
+
+    public DriveWithJoysticks() {
+        requires(Robot.drive);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    	Robot.drive.setLeftSpeed(-Robot.oi.leftStick.getY());
+    	Robot.drive.setRightSpeed(-Robot.oi.rightStick.getY());
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	Robot.drive.stop();
+    }
+
+    protected void interrupted() {
+    	end();
+    }
+}
